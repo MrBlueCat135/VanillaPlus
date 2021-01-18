0363f545ed37496ef34bdd6f4d02c7ad594ce07c
MrBlueCat135 <70517505+MrBlueCat135@users.noreply.github.com>
2021-01-18 10:20:40 +0000

    Added compressed and double compressed varieties of every single mineral type, stone and wood Code works, no textures yet

--- src/main/java/com/MrBlueCat135/vanillaPlus/registry/ModBlocks.java
+++ src/main/java/com/MrBlueCat135/vanillaPlus/registry/ModBlocks.java
@@ -17,15 +17,85 @@ public class ModBlocks {
             .requiresTool() //Cannot be obtained by hand
             .strength(5,6) //Same settings as a diamond block, hardness is related to break time and resistance is blast resistance
             .sounds(BlockSoundGroup.METAL) //Sounds made when the block is walked on
-            .luminance(15)); //Light emmited from block
+            .luminance(15)); //Light emitted from block
     public static final Block RUBY_ORE = new Block(FabricBlockSettings.of(Material.STONE)
             .breakByTool(FabricToolTags.PICKAXES, 2) //Iron Pickaxe or higher is needed to get drops
             .requiresTool() //Cannot be obtained by hand
             .strength(3,3) //Same settings as a diamond ore, hardness is related to break time and resistance is blast resistance
             .sounds(BlockSoundGroup.STONE)); //Sounds made when the block is walked on
+    public static final Block COMPRESSED_WOOD_BLOCK = new Block(FabricBlockSettings.of(Material.WOOD)
+            .breakByTool(FabricToolTags.AXES, 0) //Wooden Axe or higher is needed to break faster
+            .strength(3,6) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.WOOD)); //Sounds made when the block is walked on
+    public static final Block DOUBLE_COMPRESSED_WOOD_BLOCK = new Block(FabricBlockSettings.of(Material.STONE)
+            .breakByTool(FabricToolTags.AXES, 0) //Wooden Axe or higher is needed to break faster
+            .strength(5,6) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.WOOD)); //Sounds made when the block is walked on
+    public static final Block COMPRESSED_COBBLESTONE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE)
+            .breakByTool(FabricToolTags.PICKAXES, 1) //Stone Pickaxe or higher is needed to get drops
+            .requiresTool() //Cannot be obtained by hand
+            .strength(7,8) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.STONE)); //Sounds made when the block is walked on
+    public static final Block DOUBLE_COMPRESSED_COBBLESTONE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE)
+            .breakByTool(FabricToolTags.PICKAXES, 1) //Stone Pickaxe or higher is needed to get drops
+            .requiresTool() //Cannot be obtained by hand
+            .strength(9,10) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.STONE)); //Sounds made when the block is walked on
+    public static final Block COMPRESSED_IRON_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .breakByTool(FabricToolTags.PICKAXES, 2) //Stone Pickaxe or higher is needed to get drops
+            .requiresTool() //Cannot be obtained by hand
+            .strength(10,12) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.METAL)); //Sounds made when the block is walked on
+    public static final Block DOUBLE_COMPRESSED_IRON_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .breakByTool(FabricToolTags.PICKAXES, 2) //Stone Pickaxe or higher is needed to get drops
+            .requiresTool() //Cannot be obtained by hand
+            .strength(12,14) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.METAL)); //Sounds made when the block is walked on
+    public static final Block COMPRESSED_GOLD_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .breakByTool(FabricToolTags.PICKAXES, 2) //Stone Pickaxe or higher is needed to get drops
+            .requiresTool() //Cannot be obtained by hand
+            .strength(14,16) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.METAL)); //Sounds made when the block is walked on
+    public static final Block DOUBLE_COMPRESSED_GOLD_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .breakByTool(FabricToolTags.PICKAXES, 2) //Stone Pickaxe or higher is needed to get drops
+            .requiresTool() //Cannot be obtained by hand
+            .strength(16,18) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.METAL)); //Sounds made when the block is walked on
+    public static final Block COMPRESSED_DIAMOND_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .breakByTool(FabricToolTags.PICKAXES, 2) //Stone Pickaxe or higher is needed to get drops
+            .requiresTool() //Cannot be obtained by hand
+            .strength(18,20) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.METAL)); //Sounds made when the block is walked on
+    public static final Block DOUBLE_COMPRESSED_DIAMOND_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .breakByTool(FabricToolTags.PICKAXES, 2) //Stone Pickaxe or higher is needed to get drops
+            .requiresTool() //Cannot be obtained by hand
+            .strength(20,1200) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.METAL)); //Sounds made when the block is walked on
+    public static final Block COMPRESSED_NETHERITE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .breakByTool(FabricToolTags.PICKAXES, 3) //Stone Pickaxe or higher is needed to get drops
+            .requiresTool() //Cannot be obtained by hand
+            .strength(60,1200) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.NETHERITE)); //Sounds made when the block is walked on
+    public static final Block DOUBLE_COMPRESSED_NETHERITE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .breakByTool(FabricToolTags.PICKAXES, 3) //Stone Pickaxe or higher is needed to get drops
+            .requiresTool() //Cannot be obtained by hand
+            .strength(70,1200) //Hardness is related to break time and resistance is blast resistance
+            .sounds(BlockSoundGroup.NETHERITE)); //Sounds made when the block is walked on
 
     public static void registerBlocks() {
         Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "ruby_block"), RUBY_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "ruby_ore"), RUBY_ORE);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "compressed_wood_block"), COMPRESSED_WOOD_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "double_compressed_wood_block"), DOUBLE_COMPRESSED_WOOD_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "compressed_cobblestone_block"), COMPRESSED_COBBLESTONE_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "double_compressed_cobblestone_block"), DOUBLE_COMPRESSED_COBBLESTONE_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "compressed_iron_block"), COMPRESSED_IRON_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "double_compressed_iron_block"), DOUBLE_COMPRESSED_IRON_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "compressed_gold_block"), COMPRESSED_GOLD_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "double_compressed_gold_block"), DOUBLE_COMPRESSED_GOLD_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "compressed_diamond_block"), COMPRESSED_DIAMOND_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "double_compressed_diamond_block"), DOUBLE_COMPRESSED_DIAMOND_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "compressed_netherite_block"), COMPRESSED_NETHERITE_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Main.MOD_ID, "double_compressed_netherite_block"), DOUBLE_COMPRESSED_NETHERITE_BLOCK);
     }
 }
--- src/main/java/com/MrBlueCat135/vanillaPlus/registry/ModItems.java
+++ src/main/java/com/MrBlueCat135/vanillaPlus/registry/ModItems.java
@@ -15,10 +15,46 @@ public class ModItems {
             .group(ItemGroup.MISC)); //Creative item tab
     public static final BlockItem RUBY_ORE = new BlockItem(ModBlocks.RUBY_ORE, new Item.Settings()
             .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem COMPRESSED_WOOD_BLOCK = new BlockItem(ModBlocks.COMPRESSED_WOOD_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem DOUBLE_COMPRESSED_WOOD_BLOCK = new BlockItem(ModBlocks.DOUBLE_COMPRESSED_WOOD_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem COMPRESSED_COBBLESTONE_BLOCK = new BlockItem(ModBlocks.COMPRESSED_COBBLESTONE_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem DOUBLE_COMPRESSED_COBBLESTONE_BLOCK = new BlockItem(ModBlocks.DOUBLE_COMPRESSED_COBBLESTONE_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem COMPRESSED_IRON_BLOCK = new BlockItem(ModBlocks.COMPRESSED_IRON_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem DOUBLE_COMPRESSED_IRON_BLOCK = new BlockItem(ModBlocks.DOUBLE_COMPRESSED_IRON_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem COMPRESSED_GOLD_BLOCK = new BlockItem(ModBlocks.COMPRESSED_GOLD_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem DOUBLE_COMPRESSED_GOLD_BLOCK = new BlockItem(ModBlocks.DOUBLE_COMPRESSED_GOLD_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem COMPRESSED_DIAMOND_BLOCK = new BlockItem(ModBlocks.COMPRESSED_DIAMOND_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem DOUBLE_COMPRESSED_DIAMOND_BLOCK = new BlockItem(ModBlocks.DOUBLE_COMPRESSED_DIAMOND_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem COMPRESSED_NETHERITE_BLOCK = new BlockItem(ModBlocks.COMPRESSED_NETHERITE_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
+    public static final BlockItem DOUBLE_COMPRESSED_NETHERITE_BLOCK = new BlockItem(ModBlocks.DOUBLE_COMPRESSED_NETHERITE_BLOCK, new Item.Settings()
+            .group(ItemGroup.MISC)); //Creative item tab
 
     public static void registerItems(){
         Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "ruby"), RUBY);
         Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "ruby_block"), RUBY_BLOCK);
         Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "ruby_ore"), RUBY_ORE);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "compressed_wood_block"), COMPRESSED_WOOD_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "double_compressed_wood_block"), DOUBLE_COMPRESSED_WOOD_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "compressed_cobblestone_block"), COMPRESSED_COBBLESTONE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "double_compressed_cobblestone_block"), DOUBLE_COMPRESSED_COBBLESTONE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "compressed_iron_block"), COMPRESSED_IRON_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "double_compressed_iron_block"), DOUBLE_COMPRESSED_IRON_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "compressed_gold_block"), COMPRESSED_GOLD_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "double_compressed_gold_block"), DOUBLE_COMPRESSED_GOLD_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "compressed_diamond_block"), COMPRESSED_DIAMOND_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "double_compressed_diamond_block"), DOUBLE_COMPRESSED_DIAMOND_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "compressed_netherite_block"), COMPRESSED_NETHERITE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(Main.MOD_ID, "double_compressed_netherite_block"), DOUBLE_COMPRESSED_NETHERITE_BLOCK);
     }
 }
